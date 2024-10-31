9928f5e557ba37db0f21186fb60e78a7b4292cae
Aleksandra <ajieksandra.93@mail.ru>
2024-10-31 19:53:29 +0500

    Добавлена функция проверки баланса

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import requests
 import re
+import json
+
+def check_balance(login, password):
+    url = 'http://my3.webcom.mobi/json/balance.php'
+    headers = {'Content-type': 'text/json; charset=utf-8'}
+
+    data = {'login': login,
+            'password': password}
+    try:
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        if response.status_code == 200:
+            response_data = response.json()
+            print(response_data)
+            print(f'Баланс: {response_data['money']} руб.')
+        else:
+            print(f'Ошибка: {response.status_code}')
+    except Exception as e:
+        print(f'Произошла ошибка: {e}')
+
 
 def validate_phone_number(phone_number):
     pattern = r'^79\d{9}$'
