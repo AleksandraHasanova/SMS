35f109a764dd45324a498e94df92ddbdbfb61a53
Aleksandra <ajieksandra.93@mail.ru>
2024-10-31 19:00:13 +0500

    Добавлена обработка исключений

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import requests
 
-user = 'python24'
+user = 'A'
 password = 'parol'
-sender = 'python24'
-receiver = '79177856789'
+sender = 'python'
+receiver = '79127855656'
 text = 'Hello world!'
+try:
+    url = f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
+    print(url)
+    response = requests.get(url)
+    print(response)
 
-url = f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}'
-print(url)
-response = requests.get(url)
-print(response)
-
-if response.status_code == 200:
-    print('Успешно')
-else:
-    print(f'Ошибка {response.status_code}')
+    if response.status_code == 200:
+        print('Успешно')
+    else:
+        print(f'Ошибка {response.status_code}')
+except Exception as e:
+    print(f'Ошибка: {e}')
